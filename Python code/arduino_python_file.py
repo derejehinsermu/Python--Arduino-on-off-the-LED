e7cabb958a9480f3697df51ec546c7665426c7be
Dereje Hinsermu Senbatu <45657872+firaoll@users.noreply.github.com>
2023-01-05 14:54:11 +0300

    Update arduino_python_file.py

--- Python code/arduino_python_file.py
+++ Python code/arduino_python_file.py
@@ -1,8 +1,11 @@
+#prerequiriements
+#pip install opencv-python
+#pip install pyserial
 import time
 import serial
 import cv2
 
-arduino_ser = serial.Serial('COM4', 9600, timeout=1)
+arduino_ser = serial.Serial('COM4', 9600, timeout=1) # ensure the 'COM#' corresponds to the port that worked with the Arduino IDE.
 time.sleep(2)
 imgLedOn = cv2.imread("../Output/LedOn.png")
 imgLedOff = cv2.imread("../Output/LedOff.png")
